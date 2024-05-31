f446903d5c430763d13f8c74f3ee99200cdf7173
marlon ureta <fabrisio021@gmail.com>
2024-05-31 12:39:25 -0500

    full fronted

--- backend-pos/src/main/java/com/zegel/backendpos/dto/DtoLogin.java
+++
@@ -1,9 +0,0 @@
-package com.zegel.backendpos.dto;
-
-import lombok.Data;
-
-@Data
-public class DtoLogin {
-    private String username;
-    private String password;
-}
--- backend-pos/src/main/java/com/zegel/backendpos/security/ConstantesSeguridad.java
+++
@@ -1,6 +0,0 @@
-package com.zegel.backendpos.security;
-
-public class ConstantesSeguridad {
-    public static final long JWT_EXPIRATION_TOKEN = 300000;
-    public static final String JWT_FIRMA = "SuperTec";
-}
