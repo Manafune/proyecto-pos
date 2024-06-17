11d89bc4da47b43abfecadc6009fe28f7441e0f4
marlon ureta <fabrisio021@gmail.com>
2024-06-16 23:51:45 -0500

    refactored errors

--- fronted-pos/src/lib/products/addProducts.ts
+++ fronted-pos/src/lib/products/addProducts.ts
@@ -6,6 +6,11 @@ interface ApiResponse {
 	errors?: { message: string; code: string; name: string }[];
 }
 
+const errorMessages: { [key: string]: string } = {
+	'23505': 'El nombre del producto no puede ser duplicado',
+	'23514': 'El Stock y el Precio debe ser mayor a cero'
+};
+
 export const addProducts = async ({ products }: { products: Product[] }): Promise<ApiResponse> => {
 	try {
 		const newProducts = products.map((product) => ({
@@ -13,37 +18,30 @@ export const addProducts = async ({ products }: { products: Product[] }): Promis
 			container: product.container,
 			status: product.status,
 			stock: product.stock,
-			price: product.price,
+			price: product.price
 		}));
+
 		const { data, error } = await supabase
 			.from('product')
 			.insert([...newProducts])
 			.select();
 
-		if (error !== null) {
-			const messageResponse =
-				error.code === '23505'
-					? 'El nombre del producto no puede ser duplicado'
-					: error.code === '23514'
-						? 'El Stock y el Precio debe ser mayor a cero'
-						: '';
-			console.log(error.code);
-
+		if (error) {
+			const messageResponse = errorMessages[error.code] || 'Error desconocido';
 			const response: ApiResponse = {
 				errors: [
 					{
 						message: messageResponse,
 						code: error.code,
-						name: 'ErrorProducto',
-					},
-				],
+						name: 'ErrorProducto'
+					}
+				]
 			};
 			return response;
 		}
 
-		const response: ApiResponse = { data };
-		return response;
+		return { data };
 	} catch (error) {
-		throw new Error('Un error inesperado ocurrio al añadir producto.');
+		throw new Error('Un error inesperado ocurrió al añadir producto.');
 	}
 };
