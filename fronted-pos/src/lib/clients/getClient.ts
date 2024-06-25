7759d4fc827e168a606ee08872a9b0e3e7c0724a
marlon ureta <fabrisio021@gmail.com>
2024-06-24 23:50:04 -0500

    test

--- fronted-pos/src/lib/clients/getClient.ts
+++ fronted-pos/src/lib/clients/getClient.ts
@@ -1,17 +1,17 @@
 import supabase from '@/lib/supabase';
-import { AddressCustomer } from '@/types/clients';
-import { AddressMemberSchemaType } from '../validation/client';
+import type { AddressCustomer } from '@/types/clients';
+import type { AddressMemberSchemaType } from '../validation/client';
 export const getAllClients = async ({ current, pageSize }: { current: number; pageSize: number }) => {
 	try {
 		const pageCurrent = (current - 1) * pageSize;
 		const offset = pageCurrent + pageSize - 1;
 		const { data, error } = await supabase
 			.from('address')
-			.select(`id,street,city,state,customer(id,first_name,last_name,dni,birth_date)`)
+			.select('id,street,city,state,customer(id,first_name,last_name,dni,birth_date)')
 			.range(pageCurrent, offset);
 
 		if (error) throw new Error(error.message);
-
+		console.log(data);
 		return data as AddressCustomer[];
 	} catch (error) {
 		console.error('Error fetching products:', error);
@@ -21,12 +21,15 @@ export const getAllClients = async ({ current, pageSize }: { current: number; pa
 export const getClientById = async ({ id, timeout }: { id: string; timeout: (milliseconds: number) => AbortSignal }) => {
 	const { data: client } = await supabase
 		.from('address')
-		.select(`id,street,city,state,customer(id,first_name,last_name,dni,birth_date)`)
+		.select('id,street,city,state,customer(id,first_name,last_name,dni,birth_date)')
 		.eq('id', id)
 		.abortSignal(timeout(3000));
 	const address = client?.[0];
 	const customer = address?.customer[0];
-	const addressCustomer = { ...address, customer: { ...customer, birth_date: new Date(customer?.birth_date ?? '') } } as AddressMemberSchemaType;
+	const addressCustomer = {
+		...address,
+		customer: { ...customer, birth_date: new Date(customer?.birth_date ?? '') }
+	} as AddressMemberSchemaType;
 
 	return addressCustomer;
 };
