d5156ecd4f12fed35a1d2dfb480fb220d94ca27f
Manafune <laycanosmi@gmail.com>
2024-06-26 10:09:40 -0500

    update ticket

--- fronted-pos/src/components/Boleta/printTicket.tsx
+++ fronted-pos/src/components/Boleta/printTicket.tsx
@@ -39,41 +39,6 @@ const InvoicePdf: React.FC<{ invoice: Invoice }> = ({ invoice }) => {
 		// Agregar imagen al documento PDF
 		doc.addImage(boletaImg, 'JPEG', imgX, 5, imgWidth, imgHeight);
 
-<<<<<<< HEAD
-    doc.setFontSize(8);
-    doc.text(`Fecha: ${invoice.date}`, 10, 60);
-    doc.text(`Cliente: ${invoice.customer}`, 10, 65);
-
-    // Detalle de productos con subtotales
-    let y = 70;
-    doc.text('Item  Producto  Cantidad  Precio  Subtotal', 10, y)
-    invoice.products.forEach((product, index) => {
-      const subtotal = product.quantity * product.price;
-      doc.text(`${index + 1}.  ${product.name}     -     ${product.quantity}       ${product.price}        ${subtotal}`, 10, y+5);
-      y += 5;
-    });
-
-    // Total
-    y += 5; // Espacio adicional antes de mostrar el total
-    doc.setFontSize(10);
-    doc.text(`Total: $${invoice.total}`, textX, y, { align: 'center' });
-
-    // Abri Pdf en otra pestaña
-    const pdfUrl = doc.output('bloburl');
-    const win = window.open(pdfUrl.toString(), '_blank');
-    if (!win) {
-      alert('No se pudo abrir la vista previa del PDF. Por favor, habilite las ventanas emergentes para este sitio.');
-    }
-  };
-
-  return (
-    <div>
-      <Button variant='outline' size='icon' className='overflow-hidden rounded-full' onClick={generatePDF}>
-        <img src='https://imgur.com/3VTic93.jpeg' width={36} height={36} alt='Avatar' className='overflow-hidden rounded-full' />
-      </Button>
-    </div>
-  );
-=======
 		// Establecer estilos y contenido
 		doc.setFontSize(13);
 		doc.text('Minimarket POS', textX, 50, { align: 'center' });
@@ -126,7 +91,6 @@ const InvoicePdf: React.FC<{ invoice: Invoice }> = ({ invoice }) => {
 			</Button>
 		</div>
 	);
->>>>>>> 8791aec4bb2111459aac1e46a4062ddd4e3ea0ba
 };
 
 export default InvoicePdf;
