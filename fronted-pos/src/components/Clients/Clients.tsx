e14682e8a1572d91a3ead677222711ea7f5ae018
Manafune <laycanosmi@gmail.com>
2024-07-01 09:40:28 -0500

    culminando cliente

--- fronted-pos/src/components/Clients/Clients.tsx
+++ fronted-pos/src/components/Clients/Clients.tsx
@@ -8,39 +8,6 @@ export const Clients = () => {
 	return (
 		<Tabs defaultValue='all'>
 			<div className='flex items-center'>
-				<div className='flex gap-1'>
-					<Link
-						to='/clients'
-						search={(searchParams) => {
-							const prevSearchParams = searchParams as ClientsPagination;
-							return { ...prevSearchParams };
-						}}
-						className='inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-[#a8a29e]'
-					>
-						Todos
-					</Link>
-					<Link
-						to='/clients'
-						search={(searchParams) => {
-							const prevSearchParams = searchParams as ClientsPagination;
-							return { ...prevSearchParams };
-						}}
-						className='inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-[#a8a29e]'
-					>
-						Activo
-					</Link>
-					<Link
-						to='/clients'
-						search={(searchParams) => {
-							const prevSearchParams = searchParams as ClientsPagination;
-							return { ...prevSearchParams };
-						}}
-						className='inline-flex items-center justify-center 
-				whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-[#a8a29e]'
-					>
-						Inactivo
-					</Link>
-				</div>
 				<div className='ml-auto flex items-center gap-2'>
 					<Link
 						className='h-8 gap-1 text-sm bg-[#10b981] hover:bg-[#34d399] text-white flex flex-row items-center p-[0.5em] rounded-[0.5em]'
